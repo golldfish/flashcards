13fb55fee9d1e792d88d9d389d9f52f9c0cf8149
golldfish <a.karasinska13@gmail.com>
2022-04-04 18:37:59 +0200

    Fixed github actions

--- docker/githubactions.sh
+++ docker/githubactions.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker/docker-compose-test.yml down --remove-orphans
+docker-compose -f docker/docker-compose.yml down --remove-orphans
 #docker-compose -f docker/docker-compose-test.yml --env-file ./.env build
-docker-compose -f docker/docker-compose-test.yml run --rm app
+docker-compose -f docker/docker-compose.yml run --rm app
