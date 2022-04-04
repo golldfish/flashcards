fc78e492e1d9ecf5f3bcf0c38b64c403155a0db8
golldfish <a.karasinska13@gmail.com>
2022-04-04 19:40:09 +0200

    Added heroku

--- docker/githubactions.sh
+++
@@ -1,4 +0,0 @@
-ls
-docker-compose -f .docker/docker-compose.yml down --remove-orphans
-#docker-compose -f .github/docker/docker-compose-test.yml --env-file ./.env build
-docker-compose -f docker/docker-compose.yml run --rm app
